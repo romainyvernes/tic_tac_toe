39ed59aae7af69992b2ed8bfc59317c7a37968e0
Romain <romainyvernes@gmail.com>
2021-03-26 00:13:27 -0700

    Add line.svg, main JS completed, main CSS completed

--- script.js
+++ script.js
@@ -8,29 +8,74 @@ const Player = (marker) => {
     return {turn, play}
 };
 
-const gameBoard = (() => {
-    let boardValues = [];
+const playerDisplay = (() => {
+    const toggle = () => {
+        const players = document.querySelectorAll('.player');
+        
+        const boxShadow = 'none';
+        const transform = 'none';
+        const color = 'grey';
+        
+        const newBoxShadow = '2px 2px 1px 1px rgba(0, 0, 0, 0.5)';
+        const newTransform = 'scale(1.1)';
+        const newColor = 'black';
 
-    const render = (board) => {
+        if (game.player1.turn) {
+            players[0].style.boxShadow = newBoxShadow;
+            players[0].style.transform = newTransform;
+            players[0].style.color = newColor;
+
+            players[1].style.boxShadow = boxShadow;
+            players[1].style.transform = transform;
+            players[1].style.color = color;
+        } else {
+            players[1].style.boxShadow = newBoxShadow;
+            players[1].style.transform = newTransform;
+            players[1].style.color = newColor;
+
+            players[0].style.boxShadow = boxShadow;
+            players[0].style.transform = transform;
+            players[0].style.color = color;
+        }
+    };
+
+    return {toggle}
+})();
+
+const gameBoard = (() => {
+    let values = [];
+
+    const render = () => {
         for (let i = 0; i < 9; i++) {
+            const board = document.querySelector('#board-container');
             const boardElement = document.createElement('div');
+            
             boardElement.className = 'board-element';
             boardElement.setAttribute('data-index', i);
             boardElement.addEventListener('click', game.play);
             board.appendChild(boardElement);
 
-            boardValues.push(null);
+            values.push(null);
         }
     }
 
     const update = () => {
         const boardBoxes = document.querySelectorAll('.board-element');
         boardBoxes.forEach((box, index) => {
-            box.textContent = boardValues[index] ? boardValues[index] : '';
+            const newColor = 'rgba(0,0,0,1)';
+            const newSize = '4em';
+            
+            if (values[index]) {
+                box.textContent = values[index];
+                box.style.color = newColor;
+                box.style.fontSize = newSize;
+            } else {
+                box.textContent = '';
+            }
         });
     }
     
-    return {boardValues, render, update}
+    return {values, render, update}
 })();
 
 const game = (() => {
@@ -39,13 +84,81 @@ const game = (() => {
     const player1 = Player(_markers.shift());
     const player2 = Player(_markers.shift());
 
-    const gameover = () => {
-        // check if board has 3 symbols aligned
+    let start = false;
+
+    const end = () => {
+        game.start = false;
+    };
+
+    const checkTie = () => {
+        for (let i = 0; i < gameBoard.values.length; i++) {
+            if (!gameBoard.values[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    const checkWinner = () => {
+        const winningCombinations = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ];
+
+        let result = {
+            status: false,
+            name: null
+        };
+        
+        for (let i = 0; i < winningCombinations.length; i++) {
+            let winner = [];
+            
+            for (let j = 0; j < 3; j++) {
+                if (gameBoard.values[winningCombinations[i][j]]) {
+                    if (winner.length === 0) {
+                        winner.push(gameBoard.values[winningCombinations[i][j]]);
+                    } else {
+                        if (winner[0] === gameBoard.values[winningCombinations[i][j]]) {
+                            winner.push(gameBoard.values[winningCombinations[i][j]]);
+                        } else {
+                            break;
+                        }
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            if (winner.length === 3) {
+                result.status = true;
+                result.name = winner[0];
+                return result;
+            }
+        }
+
+        return result;
     };
     
     const play = (event) => {
-        const boxIndex = event.target.dataset.index;
+        const box = event.target;
+        const boxIndex = box.dataset.index;
         let marker;
+
+        if (!game.start) {
+            return;
+        }
+        
+        if (box.textContent !== '') {
+            return;
+        }
+
         if (player1.turn) {
             marker = player1.play();
             player1.turn = false;
@@ -56,13 +169,21 @@ const game = (() => {
             player2.turn = false;
             player1.turn = true;
         }
-        gameBoard.boardValues.splice(boxIndex, 1, marker);
+
+        gameBoard.values.splice(boxIndex, 1, marker);
         gameBoard.update();
+
+        if (checkWinner().status || checkTie()) {
+            end();
+        }
+
+        playerDisplay.toggle();
     };
 
-    return {player1, player2, play}
+    return {player1, player2, start, play}
 })();
 
-const board = document.querySelector('#board-container');
-gameBoard.render(board);
+gameBoard.render();
+game.start = true;
 game.player1.turn = true;
+playerDisplay.toggle();
